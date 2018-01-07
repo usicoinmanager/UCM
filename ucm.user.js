fd20c79b65f6941e7bf78522b99a6302577ba9e9
Kenneth Jakobsen <keja@users.noreply.github.com>
2018-01-07 16:30:34 +0100

    test of the update button

--- ucm.user.js
+++ ucm.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UCM
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  turn your data into something that makes sense
 // @author       UCM
 // @match        https://shield.usitech-int.com/*
@@ -14,7 +14,7 @@
 
 (function() {
     'use strict';
-    const version = 0.2; //test of the update function
+    const version = 0.3; //test of the update function
     const promiseSerial = funcs => funcs.reduce((promise, func) => promise.then(result => func().then(Array.prototype.concat.bind(result))), Promise.resolve([]));
     function parsePackagePopup(r){
         r = r.replace('setpopup(', '').replace(')', '').split("'").join("").split(" ").join("");
